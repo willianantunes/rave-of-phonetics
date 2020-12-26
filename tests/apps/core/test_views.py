90457e5a138911ad88ea05bf22c67d6c11752406
Willian Antunes <willian.lima.antunes@gmail.com>
2020-12-26 15:18:48 -0300

    Separating tests by language (en-US and en-GB)

--- tests/apps/core/test_views.py
+++ tests/apps/core/test_views.py
@@ -13,7 +13,7 @@ def test_should_return_home_page(client):
     assert title == "Rave of Phonetics"
 
 
-def test_should_return_home_page_with_phones_from_word(client):
+def test_should_return_home_page_with_phones_from_word_given_language_en_us(client):
     fake_data = {"text-to-be-transcribed": "something", "chosen-language": "en-us"}
     response = client.post("/", fake_data)
 
@@ -24,7 +24,7 @@ def test_should_return_home_page_with_phones_from_word(client):
     ]
 
 
-def test_should_return_home_page_with_phones_from_words(client):
+def test_should_return_home_page_with_phones_from_words_given_language_en_us(client):
     fake_data_1 = {"text-to-be-transcribed": "123 something must be avoided", "chosen-language": "en-us"}
     response_1 = client.post("/", fake_data_1)
 
@@ -42,6 +42,29 @@ def test_should_return_home_page_with_phones_from_words(client):
     assert_response(response_1)
 
     fake_data_2 = {"text-to-be-transcribed": "! 123  something must be   avoided !@#$ %", "chosen-language": "en-us"}
+    response_2 = client.post("/", fake_data_2)
+
+    assert_response(response_2)
+
+
+def test_should_return_home_page_with_phones_from_words_given_language_en_gb(client):
+    fake_data_1 = {"text-to-be-transcribed": "123 something must be avoided", "chosen-language": "en-gb"}
+    response_1 = client.post("/", fake_data_1)
+
+    def assert_response(response):
+        assert response.context["transcription"] == [
+            {"language": "en-gb", "phone": "wɒnhʌndɹɪdən twɛnti θɹiː", "word": "123"},
+            {"language": "en-gb", "phone": "sʌmθɪŋ", "word": "something"},
+            {"language": "en-gb", "phone": "mʌst", "word": "must"},
+            {"language": "en-gb", "phone": "biː", "word": "be"},
+            {"language": "en-gb", "phone": "ɐvɔɪdɪd", "word": "avoided"},
+        ]
+        assert response.status_code == 200
+        assert response.context.template_name == "core/pages/home.html"
+
+    assert_response(response_1)
+
+    fake_data_2 = {"text-to-be-transcribed": "! 123  something must be   avoided !@#$ %", "chosen-language": "en-gb"}
     response_2 = client.post("/", fake_data_2)
 
     assert_response(response_2)
