77c1f0250c3a2149f9fb59d4dffb1a3599b5c6f0
Willian Antunes <willian.lima.antunes@gmail.com>
2020-12-26 15:20:40 -0300

    Another unused script

--- tests/apps/core/service/test_phonemizer_api.py
+++
@@ -1,8 +0,0 @@
-from phonemizer import phonemize
-
-
-def test_should_qwerty():
-    text = "theoretically"
-    greg = phonemize(text, language="en-us", backend="espeak", strip=False)
-
-    assert greg is not None
