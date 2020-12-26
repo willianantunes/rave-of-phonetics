4f30c25c57e8873f8af4822afb1a73a9dc9165dd
Willian Antunes <willian.lima.antunes@gmail.com>
2020-12-26 15:45:01 -0300

    More elegant way to get the last element
    Yeah, something like "your_list.last" would be amazing

--- tests/apps/core/test_views.py
+++ tests/apps/core/test_views.py
@@ -23,7 +23,7 @@ def test_should_return_home_page_with_phones_from_word_given_language_en_us(clie
         {"word": fake_data["text-to-be-transcribed"], "phone": "sʌmθɪŋ", "language": fake_data["chosen-language"]}
     ]
     assert response.context["text"] == fake_data["text-to-be-transcribed"]
-    number_of_params = len(response.context.dicts[3])
+    number_of_params = len(response.context.dicts[-1])
     assert number_of_params == 2
 
 
@@ -42,7 +42,7 @@ def test_should_return_home_page_with_phones_from_words_given_language_en_us(cli
         assert response.status_code == 200
         assert response.context.template_name == "core/pages/home.html"
         assert response.context["text"] == fake_data_1["text-to-be-transcribed"]
-        number_of_params = len(response.context.dicts[3])
+        number_of_params = len(response.context.dicts[-1])
         assert number_of_params == 2
 
     assert_response(response_1)
@@ -68,7 +68,7 @@ def test_should_return_home_page_with_phones_from_words_given_language_en_gb(cli
         assert response.status_code == 200
         assert response.context.template_name == "core/pages/home.html"
         assert response.context["text"] == fake_data_1["text-to-be-transcribed"]
-        number_of_params = len(response.context.dicts[3])
+        number_of_params = len(response.context.dicts[-1])
         assert number_of_params == 2
 
     assert_response(response_1)
