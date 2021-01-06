5c0a9053ead00ade79079ee2054b7654fee8f131
Willian Antunes <willian.lima.antunes@gmail.com>
2021-01-06 20:06:36 -0300

    Even if I don't use it, DATABASES key setup is required

--- rave_of_phonetics/settings.py
+++ rave_of_phonetics/settings.py
@@ -69,6 +69,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "rave_of_phonetics.wsgi.application"
 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3",}}
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
