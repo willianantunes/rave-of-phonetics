a74bdde92ab4a3d2dcb098e3f4f2acc3df451899
Willian Antunes <willian.lima.antunes@gmail.com>
2021-01-17 15:36:28 -0300

    Using only CSS to change something when screen is different

--- rave_of_phonetics/apps/core/static/core/js/controllers/PageLayoutController.js
+++ rave_of_phonetics/apps/core/static/core/js/controllers/PageLayoutController.js
@@ -1,33 +1,16 @@
-import {$, $$} from "../utils/dom";
+import {$$} from "../utils/dom";
 
 export class PageLayoutController {
     constructor() {
-        this._divContainer = $('.page-header nav > div');
-        this._divRow = $('.page-header nav > div > div');
-        this._divCol = $('.page-header nav > div > div > div');
-        this._evaluateScreen()
         this._initAllEvents()
     }
 
     _initAllEvents() {
         this._applySideNavWhenMobileIsUsed();
-        window.addEventListener('resize', () => this._evaluateScreen());
     }
 
     _applySideNavWhenMobileIsUsed() {
         const elems = $$('.sidenav');
         M.Sidenav.init(elems);
-    }
-
-    _evaluateScreen() {
-        if (window.innerWidth > 992) {
-            this._divContainer.className = "container"
-            this._divRow.className = "row"
-            this._divCol.className = "col s12"
-        } else {
-            this._divContainer.className = null
-            this._divRow.className = null
-            this._divCol.className = null
-        }
     }
 }
