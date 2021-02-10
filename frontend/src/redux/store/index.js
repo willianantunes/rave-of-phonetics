f5e1b61e0a620b64fbc4c6e83173b91d5587c2d8
Willian Antunes <willian.lima.antunes@gmail.com>
2021-02-10 18:16:04 -0300

    In order to ignore warnings about non-serializable objects

--- frontend/src/redux/store/index.js
+++ frontend/src/redux/store/index.js
@@ -1,9 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import rootReducer from "../reducers"
 
 const createStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: [
+          "history/loadAllTranscriptionDetails",
+          "transcription/setText",
+          "transcription/textWasTranscribed",
+          "history/addNewTranscriptionDetails",
+          "transcription/analysingText",
+        ],
+      },
+    }),
   })
 }
 
