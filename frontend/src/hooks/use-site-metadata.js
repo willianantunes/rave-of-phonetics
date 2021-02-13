11a1350c5d03c96ee35c29afb07b04ecff5e9f49
Willian Antunes <willian.lima.antunes@gmail.com>
2021-02-13 14:38:31 -0300

    Custom hook

---
+++ frontend/src/hooks/use-site-metadata.js
@@ -0,0 +1,30 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+/**
+ * https://css-tricks.com/how-to-the-get-current-page-url-in-gatsby/
+ */
+export function useSiteMetadata() {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            social {
+              twitter
+              instagram
+              facebook
+            }
+            siteUrl
+            author {
+              name
+              summary
+            }
+          }
+        }
+      }
+    `
+  )
+  return site.siteMetadata
+}
