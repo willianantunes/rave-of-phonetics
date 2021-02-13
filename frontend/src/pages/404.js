47e6676ef4e598d9fff021348c64abe134851730
Willian Antunes <willian.lima.antunes@gmail.com>
2021-02-13 14:39:07 -0300

    Dedicated 404 page

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,29 +1,37 @@
 import React from "react"
-import { graphql } from "gatsby"
-
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby-theme-material-ui"
+import { Box, Typography } from "@material-ui/core"
+import styled from "styled-components"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const ErrorWrapper = styled(Box)`
+  & p {
+    margin-top: ${props => props.theme.spacing(3)}px;
+  }
+`
 
+const NotFoundPage = () => {
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout>
       <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <ErrorWrapper>
+        <Typography component="h1" variant="h3" align="center">
+          404
+        </Typography>
+        <Typography component="h2" variant="h4" align="center" color={"textSecondary"}>
+          Page not found{" "}
+          <span role="img" aria-label="Thinking face">
+            {" "}
+            🤔
+          </span>
+        </Typography>
+        <Typography align="center">
+          But here’s something you can always find — <Link to="/">our IPA transcription tool</Link>!
+        </Typography>
+      </ErrorWrapper>
     </Layout>
   )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
