4c1c7a0c43560e8c12b703ada2c136ab0c65fae4
Willian Antunes <willian.lima.antunes@gmail.com>
2021-02-26 11:24:25 -0300

    Configuration of Robots TXT plugin

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -26,135 +26,154 @@ const siteMetadata = {
   },
 }
 
+const plugins = [
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`,
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      path: `${__dirname}/static/assets`,
+      name: "uploads",
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/assets`,
+      name: `assets`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/blog`,
+      name: `blog`,
+    },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: "gatsby-remark-relative-images",
+          options: {
+            name: "uploads",
+          },
+        },
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 630,
+            linkImagesToOriginal: false,
+          },
+        },
+        {
+          resolve: "gatsby-remark-copy-linked-files",
+          options: {
+            destinationDir: "static/assets/",
+          },
+        },
+        {
+          resolve: `gatsby-remark-responsive-iframe`,
+          options: {
+            wrapperStyle: `margin-bottom: 1.0725rem`,
+          },
+        },
+        {
+          resolve: `@raae/gatsby-remark-oembed`,
+          options: {
+            usePrefix: false,
+            providers: {
+              include: ["Youtube", "Twitter", "Codepen"],
+              exclude: ["Reddit", "Flickr", "Instagram"],
+            },
+          },
+        },
+        `gatsby-remark-prismjs`,
+        `gatsby-remark-copy-linked-files`,
+        `gatsby-remark-smartypants`,
+        `gatsby-plugin-catch-links`,
+        `gatsby-remark-lazy-load`,
+      ],
+    },
+  },
+  `gatsby-plugin-sitemap`,
+  {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: GTM_ID,
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: GTM_INCLUDE_DEVELOPMENT,
+      // Name of the event that is triggered
+      // Defaults to gatsby-route-change
+      routeChangeEventName: "ROUTE-CHANGE",
+    },
+  },
+  `gatsby-plugin-feed`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: siteMetadata.name,
+      short_name: siteMetadata.shortName,
+      categories: ["education", "utilities", "entertainment"],
+      description: siteMetadata.description,
+      lang: "English",
+      start_url: `/`,
+      background_color: `#2196F3`,
+      theme_color: `darkseagreen`,
+      display: `minimal-ui`,
+      icon: `content/assets/rop-icon.png`,
+      icons: [
+        {
+          src: "content/assets/rop-maskable-icon.png",
+          sizes: "853x853",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+  },
+  `gatsby-plugin-react-helmet`,
+  `gatsby-plugin-offline`,
+  `gatsby-theme-material-ui`,
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-use-query-params`,
+  `gatsby-plugin-lodash`,
+  {
+    resolve: `gatsby-plugin-disqus`,
+    options: {
+      shortname: DISQUS_SHORTNAME,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-netlify-cms`,
+    options: {
+      modulePath: path.join(__dirname, "src", "cms", "cms.js"),
+      publicPath: "nebuchadnezzar",
+      manualInit: true,
+    },
+  },
+]
+
+const robotsConfiguration = {
+  resolve: "gatsby-plugin-robots-txt",
+  options: {
+    host: `${siteMetadata.siteUrl}`,
+    sitemap: `${siteMetadata.siteUrl}sitemap.xml`,
+  },
+}
+
+// I had to do this conditional because gatsby serve throws an error saying that siteUrl is undefined
+if (siteMetadata.siteUrl && siteMetadata.siteUrl.includes("www")) {
+  robotsConfiguration.options.policy = [{ userAgent: "*", allow: "/" }]
+} else {
+  robotsConfiguration.options.policy = [{ userAgent: "*", disallow: "/" }]
+}
+
+plugins.push(robotsConfiguration)
+
 module.exports = {
   siteMetadata,
-  plugins: [
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/assets`,
-        name: "uploads",
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "uploads",
-            },
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
-              linkImagesToOriginal: false,
-            },
-          },
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              destinationDir: "static/assets/",
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          {
-            resolve: `@raae/gatsby-remark-oembed`,
-            options: {
-              usePrefix: false,
-              providers: {
-                include: ["Youtube", "Twitter", "Codepen"],
-                exclude: ["Reddit", "Flickr", "Instagram"],
-              },
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-          `gatsby-plugin-catch-links`,
-          `gatsby-remark-lazy-load`,
-        ],
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: GTM_ID,
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: GTM_INCLUDE_DEVELOPMENT,
-        // Name of the event that is triggered
-        // Defaults to gatsby-route-change
-        routeChangeEventName: "ROUTE-CHANGE",
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: siteMetadata.name,
-        short_name: siteMetadata.shortName,
-        categories: ["education", "utilities", "entertainment"],
-        description: siteMetadata.description,
-        lang: "English",
-        start_url: `/`,
-        background_color: `#2196F3`,
-        theme_color: `darkseagreen`,
-        display: `minimal-ui`,
-        icon: `content/assets/rop-icon.png`,
-        icons: [
-          {
-            src: "content/assets/rop-maskable-icon.png",
-            sizes: "853x853",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-offline`,
-    `gatsby-theme-material-ui`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-use-query-params`,
-    `gatsby-plugin-lodash`,
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        shortname: DISQUS_SHORTNAME,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        modulePath: path.join(__dirname, "src", "cms", "cms.js"),
-        publicPath: "nebuchadnezzar",
-        manualInit: true,
-      },
-    },
-  ],
+  plugins,
 }
