f95658398dbe01539a2f0204757fd20a6329002c
Willian Antunes <willian.lima.antunes@gmail.com>
2021-03-13 16:39:04 -0300

    feat: new route to handle suggestion (drf class based view)

--- backend/rave_of_phonetics/urls.py
+++ backend/rave_of_phonetics/urls.py
@@ -1,13 +1,25 @@
+from django.urls import include
 from django.urls import path
+from rest_framework import routers
 
 from rave_of_phonetics.apps.core.api import api_views
 from rave_of_phonetics.apps.core.api import github_views
-from rave_of_phonetics.apps.core.api.v1 import api_views as v1
+from rave_of_phonetics.apps.core.api.v1 import api_views as api_views_v1
+from rave_of_phonetics.apps.core.api.v1 import standard_views as standard_views_v1
+
+router_v1 = routers.DefaultRouter(trailing_slash=False)
+router_v1.register("suggestions", standard_views_v1.SuggestionViewSet, basename="suggestions_v1")
+
+# Something to know worry about if this app scales out
+# https://stackoverflow.com/a/40904241/3899136
+router_unique = routers.DefaultRouter()
+router_unique.registry.extend(router_v1.registry)
 
 urlpatterns = [
     path("health-check", api_views.health_check, name="health-check"),
     path("github", github_views.index, name="github-index"),
     path("github/auth", github_views.auth, name="github-auth"),
     path("github/callback", github_views.callback, name="github-callback"),
-    path("api/v1/transcribe", v1.transcribe, name="v1/transcribe"),
+    path("api/v1/transcribe", api_views_v1.transcribe, name="v1/transcribe"),
+    path("api/v1/", include(router_v1.urls)),
 ]
--- backend/tests/int/test_urls.py
+++ backend/tests/int/test_urls.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 def test_named_urls_are_resolved_to_the_correct_endpoints(client):
     transcribe_endpoint = reverse("v1/transcribe")
     health_check_endpoint = reverse("health-check")
+    # Why did I use "{basename}-list"
+    # https://www.django-rest-framework.org/api-guide/routers/#defaultrouter
+    suggestions_v1_endpoint = reverse("suggestions_v1-list")
 
     assert transcribe_endpoint == "/api/v1/transcribe"
     assert health_check_endpoint == "/health-check"
+    assert suggestions_v1_endpoint == "/api/v1/suggestions"
