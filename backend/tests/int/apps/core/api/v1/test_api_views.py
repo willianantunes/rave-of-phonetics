22908b00dbf469f7bc1e78a3cb46495b5ec0dda3
Willian Antunes <willian.lima.antunes@gmail.com>
2021-03-13 16:36:46 -0300

    refactor: proper way to handle permissions with drf api view

--- backend/tests/int/apps/core/api/v1/test_api_views.py
+++ backend/tests/int/apps/core/api/v1/test_api_views.py
@@ -1,7 +1,23 @@
 from rave_of_phonetics.apps.core.services.recaptcha import Evaluation
 
 
-def test_should_return_400_given_no_body_is_sent(client):
+def test_should_return_403_token_is_fake_and_not_valid(client):
+    body = {"text": "rave of phonetics", "language": "en-us", "with-stress": True}
+    header = {
+        "HTTP_RECAPTCHA_TOKEN_V3": "fake-token",
+    }
+
+    response = client.post("/api/v1/transcribe", content_type="application/json", data=body, **header)
+
+    result = response.json()
+
+    assert response.status_code == 403
+    assert result["detail"] == "You are not authorized 😬"
+
+
+def test_should_return_400_given_no_body_is_sent(client, mocker):
+    mocked_verify_user_response = mocker.patch("rave_of_phonetics.apps.core.api.permissions.verify_user_response")
+    mocked_verify_user_response.return_value = Evaluation(True, "", "", 0.5, "", None)
     header = {
         "HTTP_RECAPTCHA_TOKEN_V3": "fake-token",
     }
@@ -23,22 +39,8 @@ def test_should_return_400_given_no_token_is_sent(client):
     assert result["detail"] == "Do you know the contract? 🤔"
 
 
-def test_should_return_401_token_is_fake_and_not_valid(client):
-    body = {"text": "rave of phonetics", "language": "en-us", "with-stress": True}
-    header = {
-        "HTTP_RECAPTCHA_TOKEN_V3": "fake-token",
-    }
-
-    response = client.post("/api/v1/transcribe", content_type="application/json", data=body, **header)
-
-    result = response.json()
-
-    assert response.status_code == 401
-    assert result["detail"] == "You are not authorized 😬"
-
-
 def test_should_receive_transcription(client, mocker):
-    mocked_verify_user_response = mocker.patch("rave_of_phonetics.apps.core.api.v1.api_views.verify_user_response")
+    mocked_verify_user_response = mocker.patch("rave_of_phonetics.apps.core.api.permissions.verify_user_response")
     mocked_verify_user_response.return_value = Evaluation(True, "", "", 0.5, "", None)
     body = {"text": "rave of phonetics", "language": "en-us", "with-stress": True}
     header = {
