62ff247193168aae21b8460fa9c1b2cad8420f8f
Willian Antunes <willian.lima.antunes@gmail.com>
2021-03-31 16:39:04 -0300

    feat: exclusion of duplicated entries

--- backend/scripts/start-development.sh
+++ backend/scripts/start-development.sh
@@ -3,5 +3,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py seed --create-super-user --cmu-file-location tests/resources/cmudict-0.7b.txt
+python manage.py create_dictionary
 
 python manage.py runserver 0.0.0.0:8000
