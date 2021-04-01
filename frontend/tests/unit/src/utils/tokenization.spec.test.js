8d5a445648284a162febde3adf1b5c0d1ee28053
Willian Antunes <willian.lima.antunes@gmail.com>
2021-04-01 16:18:41 -0300

    fix: extraction of words of a given phrase

--- frontend/tests/unit/src/utils/tokenization.spec.test.js
+++ frontend/tests/unit/src/utils/tokenization.spec.test.js
@@ -1,23 +1,47 @@
-import { extractWordsFromText } from "../../../../src/utils/tokenization"
+import { extractRawWordsFromText, extractWordsFromText } from "../../../../src/utils/tokenization"
 
-describe(`Words extraction`, () => {
-  test(`When with text "Rave, \r\nlive Phonetics!", then 3 words is extracted`, () => {
-    // Arrange
-    const sampleText = "Rave, live Phonetics!"
-    // Act
-    const result = extractWordsFromText(sampleText)
-    // Assert
-    expect(result).toHaveLength(3)
-    expect(result).toMatchObject(["rave", "live", "phonetics"])
+describe("Tokenization", () => {
+  describe("Words extraction", () => {
+    test(`When with text "Rave, \r\nlive Phonetics!", then 3 words is extracted`, () => {
+      // Arrange
+      const sampleText = "Rave, live Phonetics!"
+      // Act
+      const result = extractWordsFromText(sampleText)
+      // Assert
+      expect(result).toHaveLength(3)
+      expect(result).toMatchObject(["rave", "live", "phonetics"])
+    })
+
+    test(`When with text "Don't ever, if "you; please, ad-hoc 1989!", then 7 words is extracted`, () => {
+      // Arrange
+      const sampleText = `Don't ever, if "you; please, ad-hoc 1989!`
+      // Act
+      const result = extractWordsFromText(sampleText)
+      // Assert
+      expect(result).toHaveLength(7)
+      expect(result).toMatchObject(["don't", "ever", "if", "you", "please", "ad-hoc", "1989"])
+    })
   })
 
-  test(`When with text "Don't ever, if "you; please, ad-hoc 1989!", then 7 words is extracted`, () => {
-    // Arrange
-    const sampleText = `Don't ever, if "you; please, ad-hoc 1989!`
-    // Act
-    const result = extractWordsFromText(sampleText)
-    // Assert
-    expect(result).toHaveLength(7)
-    expect(result).toMatchObject(["don't", "ever", "if", "you", "please", "ad-hoc", "1989"])
+  describe("Raw words extraction", () => {
+    test("When with SCENARIO 1, then 3 words is extracted", () => {
+      // Arrange
+      const sampleText = " Rave, live Phonetics!"
+      // Act
+      const result = extractRawWordsFromText(sampleText)
+      // Assert
+      expect(result).toHaveLength(3)
+      expect(result).toMatchObject(["Rave,", "live", "Phonetics!"])
+    })
+
+    test("When with SCENARIO 2, then 5 words is extracted", () => {
+      // Arrange
+      const sampleText = ` Rave,     OF\n live \r\nPhonetics! @antunes\r\n\r\n`
+      // Act
+      const result = extractRawWordsFromText(sampleText)
+      // Assert
+      expect(result).toHaveLength(5)
+      expect(result).toMatchObject(["Rave,", "OF", "live", "Phonetics!", "@antunes"])
+    })
   })
 })
