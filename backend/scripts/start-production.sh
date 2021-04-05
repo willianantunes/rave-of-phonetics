9924e57b1ee9b6469e0bed461bc2b0b8ac2a2f06
Willian Antunes <willian.lima.antunes@gmail.com>
2021-04-05 16:36:32 -0300

    chore: some configs for production
    some configs are temporary as well

--- backend/scripts/start-production.sh
+++ backend/scripts/start-production.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+python manage.py migrate
+python manage.py seed --create-super-user --cmu-file-location tests/resources/cmudict-0.7b.txt
+python manage.py create_dictionary
+
 gunicorn -cpython:gunicorn_config -b ${DJANGO_BIND_ADDRESS}:${PORT:-$DJANGO_BIND_PORT} rave_of_phonetics.wsgi
