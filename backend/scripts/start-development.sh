e7215841728b6db8283c05d1bd11bd5911c48532
Willian Antunes <willian.lima.antunes@gmail.com>
2021-06-30 15:50:55 -0300

    chore: add comment in the script responsible to start dev backend

--- backend/scripts/start-development.sh
+++ backend/scripts/start-development.sh
@@ -8,6 +8,7 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 python manage.py seed --create-super-user --cmu-file-location tests/resources/cmudict-0.7b.txt
-#python manage.py create_dictionary
+# It delays too much, so only uncomment that if you are really willing to do that
+# python manage.py create_dictionary
 
 python manage.py runserver 0.0.0.0:${DJANGO_BIND_PORT:-$PORT}
